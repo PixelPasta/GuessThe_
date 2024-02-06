676b073841bfa04c8e72d81c1f966bfd9ca09bd4
Pixel Pasta <80410384+PixelPasta@users.noreply.github.com>
2024-02-06 22:39:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const util = require('util')
 
 app.set('view engine', 'ejs')
 app.set("views", path.join(__dirname, "views"))
-app.use(express.static(__dirname));
+app.use('/', express.static(__dirname))
+
 app.get('/', async (req, res) => {
     res.render('index')
 })
