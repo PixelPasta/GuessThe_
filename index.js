ca46f364945ddcd4fe24e69527cc7afe09d51666
Pixel Pasta <80410384+PixelPasta@users.noreply.github.com>
2024-02-06 22:08:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const fetch = require('node-fetch')
-
+const path = require('path')
 const util = require('util')
 
 app.set('view engine', 'ejs')
